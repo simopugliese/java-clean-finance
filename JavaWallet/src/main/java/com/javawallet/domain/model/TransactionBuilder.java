489fac40c541ebf33a29f07a6a9866e6f8449ab4
Simone Pugliese <166115038+simopugliese@users.noreply.github.com>
2026-02-08 22:15:27 +0100

    created TransactionBuilder

---
+++ JavaWallet/src/main/java/com/javawallet/domain/model/TransactionBuilder.java
@@ -0,0 +1,44 @@
+package com.javawallet.domain.model;
+
+import com.javawallet.domain.exception.domain.InvalidAmountException;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class TransactionBuilder {
+    private final UUID id;
+    private final Money amount;
+    private final TransactionType type;
+    private Category category;
+    private String note;
+    private LocalDateTime date;
+
+    public TransactionBuilder(Money amount, TransactionType type, LocalDateTime date) {
+        if (amount == null) throw new InvalidAmountException("Amount is required to create transaction, cannot be null");
+        if (type == null) throw new IllegalArgumentException("Type is required to create transaction, cannot be null");
+
+        this.id = UUID.randomUUID();
+        this.amount = amount;
+        this.type = type;
+        this.date = date;
+    }
+
+    public TransactionBuilder withCategory(Category category) {
+        this.category = category;
+        return this;
+    }
+
+    public TransactionBuilder withNote(String note) {
+        this.note = note;
+        return this;
+    }
+
+    public TransactionBuilder withDate(LocalDateTime date) {
+        this.date = date;
+        return this;
+    }
+
+    // Metodo finale che chiama il costruttore package-private
+    public Transaction build() {
+        return new Transaction(id, amount, type, category, date, note);
+    }
+}
