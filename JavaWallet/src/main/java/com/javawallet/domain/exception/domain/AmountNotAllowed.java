320ba03b938252a22c579fdad705de337c76a862
Simone Pugliese <166115038+simopugliese@users.noreply.github.com>
2026-02-08 23:03:44 +0100

    added new exception

---
+++ JavaWallet/src/main/java/com/javawallet/domain/exception/domain/AmountNotAllowed.java
@@ -0,0 +1,7 @@
+package com.javawallet.domain.exception.domain;
+
+public class AmountNotAllowed extends DomainException {
+    public AmountNotAllowed(String message) {
+        super(message);
+    }
+}
