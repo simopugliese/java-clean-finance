cc7aa25e20d347345ec228b7ea4eef28a7441a08
Simone Pugliese <166115038+simopugliese@users.noreply.github.com>
2026-02-09 10:48:54 +0100

    FinanceManager no longer manages the Transactions

--- JavaWallet/src/main/java/com/javawallet/application/manager/FinanceManager.java
+++ JavaWallet/src/main/java/com/javawallet/application/manager/FinanceManager.java
@@ -1,6 +1,5 @@
 package com.javawallet.application.manager;
 
-import com.javawallet.application.ports.ITransactionRepository;
 import com.javawallet.application.ports.IWalletRepository;
 import com.javawallet.domain.factory.WalletFactory;
 import com.javawallet.domain.model.Wallet;
@@ -11,17 +10,14 @@ import java.util.UUID;
 public class FinanceManager {
 
     private final IWalletRepository walletRepository;
-    private final ITransactionRepository transactionRepository;
     private final WalletFactory walletFactory;
 
-    public FinanceManager(IWalletRepository wr, ITransactionRepository tr, WalletFactory wf) {
+    public FinanceManager(IWalletRepository wr, WalletFactory wf) {
         this.walletRepository = wr;
-        this.transactionRepository = tr;
         this.walletFactory = wf;
     }
 
     public IWalletRepository getWalletRepository() { return walletRepository; }
-    public ITransactionRepository getTransactionRepository() { return transactionRepository; }
     public WalletFactory getWalletFactory() { return walletFactory; }
 
     public void addWallet(Wallet w){
